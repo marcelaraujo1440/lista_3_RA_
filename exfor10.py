4bfa335e6d12821c2f50b8dd66f9cc4a368be960
marcel araujo <marcelaraujo1440@gmail.com>
2024-04-18 17:02:39 -0300

    Update exfor10.py

--- exfor10.py
+++ exfor10.py
@@ -1,8 +1,8 @@
 lista= [2,4,7,2,3,3,1,0,2,6]
 num_rep= 0
-num_freq=0
+num_freqq=0
 for num in lista:
-    if lista.count(num)> num_freq:
-        num_freq+=1
+    if lista.count(num)> num_freqq:
+        num_freqq+=1
         num_rep=num
 print(f"O número que mais se repete é o {num_rep}")
